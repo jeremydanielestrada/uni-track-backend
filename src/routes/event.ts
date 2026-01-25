757b7f392f0df0373bb6a4d2e2a2c47938293318
Jdestrada <jeremydanielestrada2001@gmail.com>
2026-01-25 18:20:00 +0800

    feat: added event routes

---
+++ src/routes/event.ts
@@ -0,0 +1,23 @@
+import {
+  getAllEventsByCurrentAuthenticatedGovernor,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} from "../controller/event-controller";
+
+import express from "express";
+import { authenticate } from "../middleware/auth-middleware";
+
+const eventRouter = express.Router();
+
+eventRouter.get(
+  "/get",
+  authenticate,
+  getAllEventsByCurrentAuthenticatedGovernor,
+);
+
+eventRouter.post("/create", authenticate, createEvent);
+
+eventRouter.put("/update/:id", authenticate, updateEvent);
+
+eventRouter.delete("/delete/:id", authenticate, deleteEvent);
