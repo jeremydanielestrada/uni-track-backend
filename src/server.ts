482a206319b9b9a258b9a719c2ba4cdfd80ee72d
Jdestrada <jeremydanielestrada2001@gmail.com>
2026-01-29 10:04:46 +0800

    feat: added cors for cros origin integration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { authRouter } from "./routes/auth";
 import { eventRouter } from "./routes/event";
 import { studentRouter } from "./routes/student";
@@ -12,6 +13,17 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+
+console.log("CLIENT_URL:", process.env.CLIENT_URL);
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 //Routes
 app.use("/api/auth", authRouter);
